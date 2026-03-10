e2b651d4a03112fffec63f1b6997c57d2fd6e0e8
Sharif Mahmud <sharifmiah8833@gmail.com>
2026-03-10 15:50:19 +0600

    HTML file add

--- office_emp_proj/emp_app/views.py
+++ office_emp_proj/emp_app/views.py
@@ -12,6 +12,18 @@ def all_emp(request):
     return render(request, 'all_emp.html', context)
 
 def add_emp(request):
+    if request.method == 'POST':
+        print("post")
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = int(request.POST['phone'])
+        department = int(request.POST['department'])
+        role = int(request.POST['role'])
+        Employee(first_name=first_name, last_name=last_name, salary=salary, salary=salary, bonus=bonus, phone=phone, department=department, role=role).save()
+    else:
+        print("get")
     return render(request, 'add_emp.html')
 
 def remove_emp(request):
